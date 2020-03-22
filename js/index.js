ffafb2ca8d4e57f53cbe567db2b7a3df317116e7
Alexei Yukhnevich <alexei.yukhnevich94@gmail.com>
2020-03-22 18:23:38 +0300

    add feature and change game layout

--- js/index.js
+++ js/index.js
@@ -1,14 +1,19 @@
 const config = {
     classes: {
-        cell: "battle-field__cell"
+        cell: 'battle-field__cell',
+        winner: 'winner',
+        activeTitle: 'container__title--active',
+        btnReset: 'btn-reset',
     },
     signs: {
-        x: "x",
-        o: "o"
+        x: 'x',
+        o: 'o',
     },
     dependencies: {
         blocks: 9,
-        maxAmountOfCells: 9
+        maxAmountOfCells: 9,
+        minNumberForCheck: 3,
+        draw: 'draw',
     },
     matrix: [
         [0,1,2], 
@@ -19,83 +24,105 @@ const config = {
         [2,5,8],
         [0,4,8],
         [2,4,6]
-    ]
+    ],
 };
-const board = document.getElementById("board");
+
+const board = document.getElementById('board');
+const result = document.getElementById('result');
+const resetBtn = document.querySelector('.btn-reset');
+const title = document.querySelector('.container__title');
+const wrapper = document.querySelector('.wrapper');
+
 class Controller {
     constructor() {
-        this.data = {
-            active: null,
-            coordinate: null,
+        this.state = {
+            step: 0,
             boolean: true
         }
-        // this.player = 0;
-        this.step = 0;
         this.model = new Model();
         this.view = new View();
     }
+
     start() {
         this.view.startRender();
         this.controller();
     }
+
     writeIntoCell(target) {
-        this.data.coordinate = +target.getAttribute("data-value");
-        const existX = this.model.state.x.includes(this.data.coordinate);
-        const existO = this.model.state.o.includes(this.data.coordinate);
-        if(this.step % 2 === 0 && !existX && !existO) {
-            this.data.active = config.signs.x;
-            this.model.state.x.push(this.data.coordinate)
-            this.step++;
+        const coordinate = +target.getAttribute("data-value");
+        const existX = this.model.state.playerX.includes(coordinate);
+        const existO = this.model.state.playerO.includes(coordinate);
+        const { x, o } = config.signs;
+
+        if (this.state.step % 2 === 0 && !existX && !existO) {
+            this.model.state.playerX.push(coordinate);
+            this.state.step++;
+            target.innerHTML = x;
+        } else if (!existX && !existO){
+            this.model.state.playerO.push(coordinate);
+            this.state.step++;
+            target.innerHTML = o;
         }
-        else if (!existX && !existO){
-            this.data.active = config.signs.o;
-            this.model.state.o.push(this.data.coordinate)
-            this.step++;
-        }
+
         const data = this.model.check();
-        this.view.renderWinner(data);
-        target.innerHTML = this.data.active;
+        if (data instanceof Object) {
+            this.state.boolean = false;
+            this.view.renderWinner(data);
+        }
     }
+
     controller() {
-        board.addEventListener("click", (event) =>{
-            const {target} = event;
-            const {cell} = config.classes;
-            if(target.classList.contains(cell) && this.data.boolean) {
-                this.writeIntoCell(target) 
+        const { cell, btnReset } = config.classes;
+
+        wrapper.addEventListener('click', (event) => {
+            const { target } = event;
+            if (target.classList.contains(cell) && this.state.boolean) {
+                this.writeIntoCell(target);
+            } else if (target.classList.contains(btnReset)) {
+                this.reset();
             }
-        })
+        });
+    }
+
+    reset() {      
+        this.state = { step: 0, boolean: true };
+        this.model.state = { playerX: [], playerO: [] };
+        this.view.clearGame();
     }
 }
 class Model {
     constructor() {
         this.state = {
-            x:[],
-            o:[],
+            playerX: [],
+            playerO: [],
         };
     }
     check() {
-        const {x,o}  = this.state;
-        const {matrix} = config;
-        const {maxAmountOfCells} = config.dependencies;
+        const { playerX,  playerO }  = this.state;
+        const { 
+            matrix,
+            signs: { x, o },
+            dependencies: { maxAmountOfCells, minNumberForCheck, draw } 
+        } = config;
         let countX = 0;
         let countO = 0;
-        if(x.length >= 3 || o.length >= 3) {
-             for (let i=0; i<matrix.length; i++) {
-                for (let g=0; g<matrix[i].length; g++) {
-                    if (x.includes(matrix[i][g])) {
+        if (playerX.length >= minNumberForCheck || playerO.length >= minNumberForCheck) {
+             for (let i = 0; i < matrix.length; i++) {
+                for (let g = 0; g < matrix[i].length; g++) {
+                    if (playerX.includes(matrix[i][g])) {
                         countX++;
                         if (countX === matrix[i].length) {
                             return {
-                                winner: 'x',
+                                winner: x,
                                 subarray: matrix[i],
                             }
                         }
                     }
-                    else if (o.includes(matrix[i][g])) {
+                    else if (playerO.includes(matrix[i][g])) {
                         countO++;
                         if (countO === matrix[i].length) {
                             return {
-                                winner: 'o',
+                                winner: o,
                                 subarray: matrix[i],
                             }
                         }
@@ -103,53 +130,63 @@ class Model {
                 }
                 countX = 0;
                 countO = 0;
-             }
-            if (x.length + o.length === maxAmountOfCells) {
-                return {
-                    draw: 'draw'
-                } 
+            }
+            if (playerX.length + playerO.length === maxAmountOfCells) {
+                return { draw } 
             }
         }
+        return null;
     }
 }
+
 class View {
     constructor (data) {
         this.state = data;
     }
-    startRender(){
-        const {blocks} = config.dependencies;
-        const {cell} = config.classes;
+
+    startRender() {
+        const {
+            classes: { cell }, 
+            dependencies: { blocks }
+        } = config;
+
         for (let i = 0; i < blocks; i++){
-            const block = document.createElement("div");
+            const block = document.createElement('div');
             block.className = cell;
             block.setAttribute("data-value", i); 
             board.append(block);
         }
     }
-    renderWinner(data) {
-        if (typeof data === 'object') {
-            const title = document.querySelector('.title');
-            const cells = document.querySelectorAll('.battle-field__cell');
-            title.innerHTML = `
-                    <span>Winner</span>
-                    <span class='winner'>
-                        ${data.winner ? data.winner : data.draw}
-                    </span>`;
-                    if (data.winner) {
-                        data.subarray.forEach(num => {
-                            cells[num].classList.add('winner');
-                        })
-                    }
-        }
 
+    renderWinner(data) {
+        const cells = document.querySelectorAll('.battle-field__cell');
+        const { activeTitle } = config.classes;
+        const outcomeOfGame = data.winner ? data.winner : data.draw;
+
+        title.classList.add(activeTitle);
+        result.textContent = outcomeOfGame;;
+
+        if (data.winner) {
+            data.subarray.forEach(num => cells[num].classList.add('outcome'));
+        }
     }
-    render() {
-    
-    }
+
+    clearGame() {
+        const cells = document.querySelectorAll('.battle-field__cell');
+        const { activeTitle } = config.classes;
+
+        title.classList.remove(activeTitle);
+		cells.forEach(cell => {
+            cell.textContent = '';
+            if (cell.classList.contains('outcome')) {
+                cell.classList.remove('outcome'); 
+            }
+        });
+	}
+
 }
 const app = new Controller();
-app.start("div");
-
+app.start();
 
 
 
